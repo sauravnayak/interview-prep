cd26b08097d04a3ae41cc35f83dbd21a5f769b7d
saurav <sauravnayak999@gmail.com>
2026-07-29 03:07:49 +0530

    Adding Screenshot capture on failure

--- src/test/java/com/interview/prep/utility/CustomListeners.java
+++ src/test/java/com/interview/prep/utility/CustomListeners.java
@@ -3,15 +3,24 @@ package com.interview.prep.utility;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import static com.interview.prep.web.DriverFactory.getDriver;
 
 
 public class CustomListeners implements ITestListener {
 
     private static final Logger log = LogManager.getLogger(CustomListeners.class);
+    private static final String SCREENSHOT_DIR = "target/screenshots/";
 
     @Override
     public void onTestStart(ITestResult result){
@@ -29,6 +38,21 @@ public class CustomListeners implements ITestListener {
         if (result.getThrowable() != null) {
             log.error("Reason: ", result.getThrowable());
         }
+        String timestamp =new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File src= ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+        File directory = new File(System.getProperty("user.dir"), "target/screenshots");
+
+        // 3. Force create the folder structure if it doesn't exist yet
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        File dest = new File(directory,result.getMethod().getMethodName()+timestamp+".png");
+
+        try {
+            FileUtils.copyFile(src,dest);
+        } catch (IOException e) {
+            System.out.println("Failed to capture page screenshot: " + e.getMessage());        }
+
     }
 
     @Override
